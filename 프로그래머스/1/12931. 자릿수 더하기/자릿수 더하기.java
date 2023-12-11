84a055f9a67b4f7feb6dd6e7c487e6acf8243bef
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2023-12-11 23:34:27 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,10 +2,9 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-         if (n == 0) {
-            return 0;
-        } else {
+        while(n!=0){
             return n % 10 + solution(n / 10);
         }
+        return n;
     }
 }
