7ac98d2ed7543ef18f0cdadde2c80f933e89db66
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2023-12-30 22:58:02 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,20 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int[] numberArr = {1,2,3,4,5,6,7,8,9}; 
-        List<Integer> list = new ArrayList<>();    
+        int answer = 45;
         for(int i=0;i<numbers.length;i++){
-            list.add(numbers[i]);
+           answer -= numbers[i];
         }
         
-        System.out.println(list);
-        
-        for(int i= 0; i<numberArr.length;i++){
-            if(!list.contains(numberArr[i])){
-                answer += numberArr[i];
-            }
-        }
+       
         return answer;
     }
 }
