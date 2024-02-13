f58ee93ba8804b16bb1a23a69bd61c79d8a45001
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-13 14:28:28 +0900

    [Bronze II] Title: 단어의 개수, Time: 320 ms, Memory: 27192 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] split = br.readLine().split(" ");
+        Object[] array = Arrays.stream(split).filter(v -> !v.isBlank()).toArray();
+        bw.write(array.length+"\n");
+        bw.flush();
+    }
+}
