cdc3449a5f13695bc30c2a4a5d9ae136caa8fef1
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-14 22:21:33 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 144 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Silver V/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver V/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,36 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int lineCount = Integer.parseInt(br.readLine());
         int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            String word = scanner.next();
-            if (isGroupWord(word)) {
+        for (int i = 0; i < lineCount; i++) {
+            String sentence = br.readLine();
+            if (isGroup(sentence)) {
                 count++;
             }
-        }
 
-        System.out.println(count);
+
+        }
+        bw.write(count+"\n");
+
+        bw.flush();
     }
 
-    public static boolean isGroupWord(String word) {
+    private static boolean isGroup(String sentence) {
         boolean[] visited = new boolean[26];
+        char[] c = sentence.toCharArray();
         int prev = -1;
-
-        for (char c : word.toCharArray()) {
-            if (prev != c) {
-                if (!visited[c - 'a']) {
-                    visited[c - 'a'] = true;
-                    prev = c;
-                } else {
+        for (int i = 0; i < c.length; i++) {
+            if (prev != c[i]) {
+                if (!visited[c[i] - 'a']){
+                    visited[c[i]-'a'] = true;
+                    prev = c[i];
+                }else {
                     return false;
                 }
             }
         }
-
         return true;
     }
 }
