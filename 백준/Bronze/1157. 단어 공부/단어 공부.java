fb3472d9652fde9a347c9a88bd933648f7896913
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-14 12:51:40 +0900

    [Bronze I] Title: 단어 공부, Time: 296 ms, Memory: 21408 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -3,54 +3,28 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] sentenceArr = br.readLine().toUpperCase().split("");
-        List<String> elements = Arrays.stream(sentenceArr).distinct().collect(Collectors.toList());
-
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        for(int i=0;i<elements.size();i++){
-            hashMap.put(elements.get(i),0);
+        String upperCasedSentence = br.readLine().toUpperCase();
+        int[] alphabets = new int[26];
+        for (int i = 0; i < upperCasedSentence.length(); i++) {
+            alphabets[upperCasedSentence.charAt(i) - 'A']++;
         }
-        for(int j=0;j<elements.size();j++){
-            for(int i=0;i<sentenceArr.length;i++){
-                if(sentenceArr[i].equals(elements.get(j))){
-                    Integer count = hashMap.get(elements.get(j));
-                    count++;
-                    hashMap.put(elements.get(j),count);
-                }
+
+        int max = -1;
+        char answer = '?';
+        for (int i = 0; i < alphabets.length; i++) {
+            if (alphabets[i] > max) {
+                max = alphabets[i];
+                answer = (char) (i + 'A');
+            } else if (alphabets[i] == max) {
+                answer = '?';
             }
         }
-
-        int max=0;
-        String sentence = null;
-        for(int i=0;i<elements.size();i++){
-            if(hashMap.get(elements.get(i))>max){
-                max = hashMap.get(elements.get(i));
-                sentence = elements.get(i);
-            }
-        }
-        int cnt=0;
-        for(int i=0;i<elements.size();i++){
-           if(hashMap.get(elements.get(i))==max){
-               cnt++;
-           }
-        }
-
-
-        if(cnt==1){
-            bw.write(sentence+"\n");
-        }else {
-            bw.write("?"+"\n");
-        }
-
+        bw.write(answer + "\n");
 
         bw.flush();
     }
