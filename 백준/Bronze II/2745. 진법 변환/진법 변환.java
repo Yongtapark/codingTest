7ced9cbf2d4c0b650de476966a4ca914d951aea3
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-15 12:40:41 +0900

    [Bronze II] Title: 진법 변환, Time: 148 ms, Memory: 15940 KB -BaekjoonHub

---
+++ 백준/Bronze II/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String B = st.nextToken();
+        Integer N = Integer.parseInt(st.nextToken());
+        bw.write(Integer.parseInt(B, N)+"\n");
+        bw.flush();
+    }
+}
