052f0bebad18d03b3e6bd012a670e5f380811e33
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-15 16:41:01 +0900

    [Bronze II] Title: 벌집, Time: 156 ms, Memory: 16196 KB -BaekjoonHub

---
+++ 백준/Bronze II/2292. 벌집/벌집.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 1) {
+            bw.write("1\n");
+        } else {
+            int origin = 1, count = 1;
+            while (N > origin) {
+                origin += 6 * count; 
+                count++;
+            }
+            bw.write(count + "\n");
+        }
+        bw.flush();
+    }
+}
