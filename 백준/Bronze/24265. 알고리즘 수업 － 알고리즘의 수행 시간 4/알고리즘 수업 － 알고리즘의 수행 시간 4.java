ec01537de34c7113d5967d86e4b098b54f4f14a4
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-18 01:07:18 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 132 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      
+        int n = Integer.parseInt(br.readLine());
+
+   
+        long count = (long)n * (n - 1) / 2;
+
+
+        System.out.println(count);
+
+     
+        System.out.println(2);
+    }
+}
