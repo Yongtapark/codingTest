803532bfe9024b50c0ce2581d478c262c916fce6
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-20 10:51:24 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1844 ms, Memory: 379508 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] numbers = new int[20000001];
+
+        for (int i = 0; i < N; i++) {
+            numbers[Integer.parseInt(br.readLine())+10000]++;
+        }
+
+        for(int i=0;i<20000001;i++){
+            if(numbers[i]!=0){
+                for(int j=0; j<numbers[i];j++){
+                    bw.write(i-10000+"\n");
+                }
+            }
+        }
+        bw.flush();
+    }
+}
