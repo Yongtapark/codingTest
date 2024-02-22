7d624aa35d2c0c84ff6e066de1d362b8e07b992c
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-22 18:49:57 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 132 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,32 +1,59 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int count = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Queue<Integer> queue = new LinkedList<>();
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < N; i++) {
-            queue.offer(Integer.parseInt(st.nextToken()));
-        }
         
-        int waitingNumber = 1;
-        while (!queue.isEmpty() || !stack.isEmpty()) {
-            if (!queue.isEmpty() && queue.peek() == waitingNumber) {
-                queue.poll();
-                waitingNumber++;
-            } else if (!stack.isEmpty() && stack.peek() == waitingNumber) {
-                stack.pop();
-                waitingNumber++;
-            } else if (!queue.isEmpty()) {
-                stack.push(queue.poll());
+        int waitTurn = 1;
+        Stack<Integer> tempWaitLine = new Stack<>();
+        for (int i = 0; i < count; i++) {
+            int turn = Integer.parseInt(st.nextToken());
+            waitTurn = searchFromTempWait(tempWaitLine, waitTurn);
+            if (turn == waitTurn) {
+                waitTurn++;
             } else {
-                System.out.println("Sad");
-                return;
+                tempWaitLine.push(turn);
             }
+            waitTurn = searchFromTempWait(tempWaitLine, waitTurn);
+
         }
-        System.out.println("Nice");
+
+        if (tempWaitLine.isEmpty()) {
+            sb.append("Nice").append("\n");
+        } else {
+            sb.append("Sad").append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
+
+    private static int searchFromTempWait(Stack<Integer> tmpLine, int waitTurn) {
+        while (!tmpLine.isEmpty()) {
+            if (waitTurn == tmpLine.peek()) {
+                tmpLine.pop();
+                waitTurn++;
+            } else {
+                break;
+            }
+
+        }
+        return waitTurn;
+    }
+
 }
+
+
