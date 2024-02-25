e35b9a8ac641a1b0666f001862005e4a414a51e0
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-25 13:00:26 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1032 ms, Memory: 88920 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
@@ -9,6 +11,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
@@ -35,9 +38,15 @@ public class Main {
 
         dfs(r);
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <=n ; i++) {
-            System.out.println(visitCount[i]);
+            sb.append(visitCount[i]).append("\n");
         }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+        
 
 
     }
