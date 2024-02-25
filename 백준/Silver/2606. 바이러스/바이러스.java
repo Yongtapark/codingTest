4771e7c445743d6d67f7f48d50b5ccc5efef32de
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-25 15:03:47 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -8,39 +8,35 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        int R = 1;
+       // StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        adjList=new ArrayList[N+1];
-        for(int i=1;i<=N;i++){
-            adjList[i]=new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        adjList = new ArrayList[n + 1];
+        isVisited = new boolean[n + 1];
+        visitedOrder = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adjList[i] = new ArrayList<>();
         }
-
-        for(int i=0;i<M;i++){
-            StringTokenizer st =new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             adjList[a].add(b);
             adjList[b].add(a);
         }
+        dfs(1);
+        //bfs(1);
 
-        for(int i=1;i<=N;i++){
-            Collections.sort(adjList[i]);
-        }
+        sb.append(visitCount).append("\n");
 
-        isVisited =new boolean[N+1];
-        dfs(R);
-        /*for (boolean b : isVisited) {
-            if(b){
-                count++;
-            }
-        }*/
-        sb.append(count-1).append("\n");
 
 
         bw.write(sb.toString());
@@ -48,24 +44,37 @@ public class Main {
         bw.close();
         br.close();
 
+
     }
 
     static ArrayList<Integer>[] adjList;
     static boolean[] isVisited;
-    static StringBuilder sb =new StringBuilder();
-    static int count=0;
+    static int visitCount = 0;
+    static int[] visitedOrder;
 
     static void dfs(int v){
-        if(isVisited[v]){
-            return;
-        }
-        count++;
         isVisited[v]=true;
         for(int i : adjList[v]){
-            dfs(i);
+            if(!isVisited[i]){
+                dfs(i);
+                visitCount++;
+            }
         }
     }
 
-
-
+    static void bfs(int start) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        isVisited[start] = true;
+        while (!queue.isEmpty()){
+            Integer v = queue.poll();
+            for(int i : adjList[v]){
+                if(!isVisited[i]){
+                    queue.offer(i);
+                    isVisited[i]=true;
+                    visitCount++;
+                }
+            }
+        }
+    }
 }
