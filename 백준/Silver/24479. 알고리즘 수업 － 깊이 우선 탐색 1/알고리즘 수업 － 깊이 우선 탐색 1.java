37b8db9dd718feac3a8177cec06c08003aa800d5
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-25 14:19:30 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 980 ms, Memory: 89832 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -5,67 +5,64 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int R = Integer.parseInt(st.nextToken());
-        tmp = new int[N+1];
-
-        adjList=new ArrayList[N+1];
-        for(int i=1;i<=N;i++){
-            adjList[i]=new ArrayList<>();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        adjList = new ArrayList[n + 1];
+        isVisited = new boolean[n + 1];
+        visitedOrder = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adjList[i] = new ArrayList<>();
         }
-
-        for(int i=0;i<M;i++){
-            st =new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             adjList[a].add(b);
             adjList[b].add(a);
         }
-
-        for(int i=1;i<=N;i++){
+        for (int i = 1; i <=n ; i++) {
             Collections.sort(adjList[i]);
         }
 
-        isVisited =new boolean[N+1];
-        dfs(R);
-        for(int i=1;i<tmp.length;i++){
-            sb.append(tmp[i]).append("\n");
-        }
+        dfs(r);
 
+        for (int i = 1; i <=n ; i++) {
+            sb.append(visitedOrder[i]).append("\n");
+        }
 
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
 
+
     }
 
     static ArrayList<Integer>[] adjList;
     static boolean[] isVisited;
-    static StringBuilder sb =new StringBuilder();
-    static int[] tmp;
-    static int count=1;
+    static int visitCount = 1;
+    static int[] visitedOrder;
 
-    static void dfs(int v){
-        if(isVisited[v]){
+   static void dfs(int v) {
+        if (isVisited[v]) {
             return;
         }
-        isVisited[v]=true;
-        tmp[v]=count++;
-        for(int i : adjList[v]){
+        isVisited[v] = true;
+        visitedOrder[v]=visitCount++;
+        for (int i : adjList[v]) {
             dfs(i);
         }
     }
-
 
 
 }
