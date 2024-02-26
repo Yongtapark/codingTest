74535ac498d3c60a8585a1380933f380c4450701
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2024-02-26 13:50:39 +0900

    [Silver I] Title: 나이트의 이동, Time: 372 ms, Memory: 176180 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int repeatCount = Integer.parseInt(br.readLine());
+        for (int i = 0; i < repeatCount; i++) {
+            oneSide = Integer.parseInt(br.readLine());
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int currentY = Integer.parseInt(st.nextToken());
+            int currentX = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            int targetY = Integer.parseInt(st.nextToken());
+            int targetX = Integer.parseInt(st.nextToken());
+
+            moveCount = new int[oneSide + 1][oneSide + 1];
+            isVisited = new boolean[oneSide + 1][oneSide + 1];
+
+            int[][] map = new int[oneSide + 1][oneSide + 1];
+
+            int[] current = {currentY, currentX};
+            int[] target = {targetY, targetX};
+            int answer = bfs(current, target);
+            sb.append(answer).append("\n");
+
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static boolean[][] isVisited;
+    static int[][] moveCount;
+    static int oneSide;
+
+    private static int bfs(int[] current, int[] target) {
+        LinkedList<int[]> queue = new LinkedList<int[]>();
+        int curY = current[0];
+        int curX = current[1];
+        queue.offer(new int[]{curY, curX});
+        moveCount[curY][curX] = 1;
+        isVisited[curY][curX] = true;
+        while (!queue.isEmpty()) {
+            int[] next = queue.poll();
+            int nextY = next[0];
+            int nextX = next[1];
+            if (nextY == target[0] && nextX == target[1]) {
+
+                return moveCount[nextY][nextX] - 1;
+            }
+            int[] moveUpAndDown = {2, 2, -2, -2, -1, 1, -1, 1};
+            int[] moveLeftAndRight = {1, -1, 1, -1, -2, -2, 2, 2};
+            for (int i = 0; i < 8; i++) {
+                int moveY = nextY + moveUpAndDown[i];
+                int moveX = nextX + moveLeftAndRight[i];
+                if (moveY >= 0 && moveY < oneSide && moveX >= 0 && moveX < oneSide) {
+                    if (!isVisited[moveY][moveX] && moveCount[moveY][moveX] == 0) {
+                        isVisited[moveY][moveX] = true;
+                        moveCount[moveY][moveX] = moveCount[nextY][nextX] + 1;
+                        queue.offer(new int[]{moveY, moveX});
+                    }
+                }
+            }
+
+
+        }
+        return -1;
+
+    }
+
+}
